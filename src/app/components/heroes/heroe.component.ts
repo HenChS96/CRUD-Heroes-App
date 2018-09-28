efbdb27926e8f9ad4b9fabadc2347b6c2d197a06
Aaron Chavez <achavez@corporaciondpt.com>
2018-09-27 19:36:47 -0600

    7:15 to finished lesson  8 and fixed errors on rxjs

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroe',
@@ -15,13 +17,17 @@ export class HeroeComponent implements OnInit {
     house: 'Marvel'
   }
 
-  constructor() { }
+  constructor( private _heroeService: HeroesService ) { }
 
   ngOnInit() {
   }
 
   save(){
-    
+    console.log(this.heroe);
+
+    this._heroeService.newHeroe(this.heroe).subscribe(data => {
+
+    })
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Heroe } from '../interfaces/heroe.interface';
+
+import { Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs';
+
+import 'rxjs/Rx';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  constructor() { }
+  url = "https://heroesappcrud-734b0.firebaseio.com/Heroes.json";
+
+  constructor(private http: Http) { }
+
+  newHeroe(heroe: Heroe): Observable<any> {
+
+    let body = JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(this.url, body, { headers })
+      .map(res => {
+        console.log(res.json());
+        return res.json();
+      });
+
+  }
 }
